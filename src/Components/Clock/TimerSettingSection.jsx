4a31881dca43d418600594325b51cc4d881a638a
wsameer <dev.wsameer@gmail.com>
2021-09-18 15:09:08 +0530

    feat: adds TimerSettingSection component

---
+++ src/Components/Clock/TimerSettingSection.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+const TimerSettingSection = ({
+    titleId,
+    title,
+    length,
+    lengthId,
+    primaryBtn,
+    secondaryBtn,
+}) => {
+    return (
+        <div className="col me-2">
+            <TimerSettingTitle id={titleId} title={title} />
+            <div className="row mt-2 align-items-center">
+                <TimerSetting
+                    className="col btn btn-success"
+                    data={primaryBtn}
+                    value="+"
+                />
+                <div className="col" id={lengthId}>
+                    {length}
+                </div>
+                <TimerSetting
+                    className="col btn btn-danger"
+                    data={secondaryBtn}
+                    value="-"
+                />
+            </div>
+        </div>
+    );
+};
+
+export default TimerSettingSection;
