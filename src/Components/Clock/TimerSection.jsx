1cd310175d5875a8ce2f8bf60a6b7b565049eda4
wsameer <dev.wsameer@gmail.com>
2021-09-18 18:31:36 +0530

    refactor: adds proptypes

--- src/Components/Clock/TimerSection.jsx
+++ src/Components/Clock/TimerSection.jsx
@@ -1,35 +1,35 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
+import PropTypes from "prop-types";
 import TimerController from "./TimerController";
 
-const TimerSection = ({ activeTimer, timer, playPauseTimer, resetTimer }) => {
-    const [time, setTime] = useState(timer);
-
-    useEffect(() => {
-        let minutes = String(Math.floor(timer / 60));
-        let seconds = String(timer % 60);
-        if (seconds?.length === 1) seconds = "0" + seconds;
-        if (minutes?.length === 1) minutes = "0" + minutes;
-        setTime(minutes + ":" + seconds);
-    }, [timer]);
-
-    return (
-        <div className="col card text-center mt-4">
-            <div className="card-body">
-                <h3 id="timer-label" className="card-title text-uppercase">
-                    {activeTimer}
-                </h3>
-                <h1 className="text-jumbo" id="time-left">
-                    {time}
-                </h1>
-            </div>
-            <div className="card-footer">
-                <TimerController
-                    resetTimer={resetTimer}
-                    playPauseTimer={playPauseTimer}
-                />
-            </div>
+const TimerSection = ({
+    activeTimer,
+    resetTimer,
+    getCurrentTime,
+    startTimer,
+    textColor,
+}) => (
+    <div className="col card text-center mt-4">
+        <div className="card-body">
+            <h3 id="timer-label" className="card-title text-uppercase">
+                {activeTimer}
+            </h3>
+            <h1 className={`text-jumbo ${textColor}`} id="time-left">
+                {getCurrentTime()}
+            </h1>
         </div>
-    );
+        <div className="card-footer">
+            <TimerController resetTimer={resetTimer} startTimer={startTimer} />
+        </div>
+    </div>
+);
+
+TimerSection.propTypes = {
+    activeTimer: PropTypes.string,
+    resetTimer: PropTypes.func,
+    getCurrentTime: PropTypes.func,
+    startTimer: PropTypes.func,
+    textColor: PropTypes.string,
 };
 
 export default TimerSection;
