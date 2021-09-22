6d0a14849b1c5c4d130452cde730700a44374b50
wsameer <dev.wsameer@gmail.com>
2021-09-22 19:42:58 +0530

    test: adds snapshot test for app component

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,7 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test("renders the app properly", () => {
+    const { container } = render(<App />);
+    expect(container).toMatchSnapshot();
 });
